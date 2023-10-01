06d67b00011b3f15da1b2485e127784d7cc0426a
Caner <caneryeniyurt@gmail.com>
2023-10-01 15:45:22 +0200

    week1exercise

--- weeks/week-1/exercises/index.js
+++ weeks/week-1/exercises/index.js
@@ -11,8 +11,18 @@ console.log(`Welcome to ${cafeName}! What would you like to order today?`);
 /*
 2)
 Create a variable that stores the price of a coffee.
+*/
+let priceOfCoffe = 5;
+/*
 Create a variable that stores how many coffees the customer wants.
+*/
+let numberOfCoffe = 2;
+/*
 Print out the total price such as "There you go, that'll be 10 euros"
+*/
+
+console.log(`There you go, that'll be ${numberOfCoffe*priceOfCoffe} euros`);
+/*
 Where 10 is replaced by the calculation of the total price.
 */
 // Good practice Approach
@@ -25,7 +35,8 @@ Create a variable that stores a boolean.
 Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
-
+let goodCoffe=false;
+console.log(`You said this coffee is the best, that was actually ${goodCoffe}`);
 /*
 4)
 Create a variable called cafeGuests, that shows us how many cafeGuests we have.
@@ -34,7 +45,11 @@ Print it out.
 assign it a new value.
 Print it out. => This should give you the new value.
 */
+let cafeGuests= 10;
 
+cafeGuests= 12;
+console.log(`${cafeGuests}`);
+/*
 /*
 5)
 Create a variable called maxGuests, that shows us how many guests are allowed in the cafe.
@@ -44,12 +59,17 @@ assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
 
-//maxGuests = "I also love rock n roll!";
+const maxGuests = 15;
+maxGuests = 12;
+console.log(`${maxGuests}`);
+
 /*
 6)
 Create a variable that stores a string.
 Print out that string in only UPPERCASE letters.
 */
+let x = "String";
+console.log.(x.toUpperCase);
 
 /*
 7)
